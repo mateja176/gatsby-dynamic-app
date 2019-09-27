52f6a9f094a1aa8a6e215c630f30eb01dd7a0138
mateja <ma.petrovic@levi9.com>
2019-09-27 15:37:26 +0200

    feat(node): adds onCreatePage

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,35 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+/**
+ * source https://www.gatsbyjs.org/docs/client-only-routes-and-user-authentication/
+ * @typedef Page
+ * @property {string} config.page.path
+ * @property {string} config.page.matchPath
+ */
+
+/**
+ * @callback CreatePage
+ * @param {Page} page
+ * @returns {void}
+ */
+
+/**
+ * @param {Object} config
+ *
+ * @param {Page} config.page
+ *
+ * @param {Object} config.actions
+ * @param {CreatePage} config.actions.createPage
+ *
+ * @returns {Promise<void>}
+ */
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions;
+
+  if (page.path.match(/^\/app/)) {
+    page.matchPath = '/app/*';
+
+    createPage(page);
+  }
+};
